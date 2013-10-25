3c357d285b3969addce21b4566b0a2b491da69ff
slopen <kostrov@gmail.com>
2013-10-25 17:19:42 +0400

    cdn paths fix

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 require.config({
   paths: {
 
-    "jquery"                : '//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min',
-    "underscore"            : '//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.2.1/lodash.compat.min',
-    "backbone"              : '//cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.9-amdjs/backbone-min',
+    "jquery"                : 'https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min',
+    "underscore"            : 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.2.1/lodash.compat.min',
+    "backbone"              : 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.9-amdjs/backbone-min',
 
     // plugin    
     "swipe"		    : 'swipe',        
